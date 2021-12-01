f3a79c924a04b1ecf6bcf2c05a587887e9bd6105
FrancescaDiFilippo <francescadifilippo97@gmail.com>
2021-12-01 10:42:48 +0100

    Update aggiunta variabili_fra.R

--- FRANCESCA/aggiunta variabili_fra.R
+++ FRANCESCA/aggiunta variabili_fra.R
@@ -3,80 +3,56 @@
 setwd('C:/Users/franc/Desktop/NONPA/PROGETTO')
 dataset<- read.csv("dataset.csv")
 
-
+library(dplyr)
 dati <- dataset[1:1000,]
-colnames(dati)
-dati$timedel
-attach(data)
-#"timestamp"  "cuebiq_id" "device"  "latitude" "longitude" "datetime"  "stopping"  "journey_id" "stationary_id"
-dati_totaltime <- dati %>% select(time=first(timestamp ), tot_time= sum(timedelta), cuebiq_id ,device , lat_init=first( latitude),long_init= first(longitude),time_init=first(datetime)) 
-                     %>% group_by(cuebiq_id,journey_id)
-timedelta <- as.integer(timedelta)
-dati_totaltime <- dati %>% select( cuebiq_id,journey_id,tot_time=sum(as.numeric(timedelta)))%>% group_by(cuebiq_id,journey_id)
+# write.csv2( dati,'dataset_100.csv')
+# colnames(dati)
+# dati<- read.csv("dataset_100.csv", header=T)
 
-which(is.na(timedelta)==TRUE)
-
+ attach(dati)
 dati <- dati[,-11]
 
-dati <- dataset[,-dim(dataset)[2]]
+#dati <- dataset[,-dim(dataset)[2]]
 dati[which(is.na(dati[,7])==TRUE),7] <- 0
 dati[which(is.na(dati[,8])==TRUE),8] <- 0
 
-#bisogna mettere a 0 i timedelta e spacedelta di tutte le prime osservazioni di ciascun journey_id
-j.id <- unique(data$journey_id) #dataset ordinato per journey ID
-first<- NULL #first è il vettore degli indici delle prime osservazioni per ciascun journey_id
-for (i in 1:length(j.id)){
-  temp <- subset(data, journey_id==j.id[i])
-  temp2 <- cbind(temp[1,1], temp[1,10])
-  first<-rbind(first, which(data$timestamp==temp2[1] & data$journey_id==temp2[2]))
-}
-data[first,7:8] <- 0
+dati[which(stopping==TRUE),7] <- 0
+dati[which(stopping==TRUE),8] <- 0
 
-data <- data[1:4000,] #dataset completo troppo pesante
 
-#calcolo quanti viaggi ci sono stati al giorno 
-range.time <- cbind(1581894000,1582498799)
-j.per.day <- NULL
-for (i in 1:7){
-  j.per.day [i] <- length(unique(data[which(data$timestamp>=range.time[1]+(i-1)*24*60*60 & data$timestamp<range.time[1]+i*24*60*60),dim(data)[2]]))
-}
+# 
+# #bisogna mettere a 0 i timedelta e spacedelta di tutte le prime osservazioni di ciascun journey_id
+# j.id <- unique(dati$journey_id) #dataset ordinato per journey ID
+# first<- NULL #first è il vettore degli indici delle prime osservazioni per ciascun journey_id
+# for (i in 1:length(j.id)){
+#   temp <- subset(dati, journey_id==j.id[i])
+#   temp2 <- cbind(temp[1,1], temp[1,10])
+#   first<-rbind(first, which(dati$timestamp==temp2[1] & dati$journey_id==temp2[2]))
+# }
+# dati[first,7:8] <- 0
 
-#quanti utenti si sono spostati al giorno
-people.per.day <- NULL
-for (i in 1:7){
-  people.per.day[i] <- length(unique(data[which(data$timestamp>=range.time[1]+(i-1)*24*60*60 & data$timestamp<range.time[1]+i*24*60*60),2]))
-}
+dati_totaltime <- dati %>%  group_by(cuebiq_id,journey_id) %>% summarise(tot_time = sum(timedelta))
+dati_totalspace <- dati %>%  group_by(cuebiq_id,journey_id) %>% summarise(tot_space = sum(spacedelta))
+dati_tot  <- dati %>%  group_by(cuebiq_id,journey_id) %>% summarise(tot_time = sum(timedelta),tot_space = sum(spacedelta))
+dati_tot$mean_speed <- (dati_tot$tot_space/dati_tot$tot_time)/1000*3600
+dati_meanvalues <- dati_tot %>%  group_by(cuebiq_id) %>% summarise(mean_time =mean(tot_time), mean_space=mean(tot_space))
+dati_complete <- left_join(dati_tot,dati_meanvalues, by= 'cuebiq_id')
+ii <- which(dati_complete$mean_speed > 30)
 
-#distanza percorsa al giorno
-dist.per.day <- rep(0,7)
-for (i in 1:7){
-  journeys <- unique(data[which(data$timestamp>=range.time[1]+(i-1)*24*60*60 & data$timestamp<range.time[1]+i*24*60*60),dim(data)[2]])
-  for(j in 1:length(journeys)){
-    dist.per.day[i] <- dist.per.day[i] + sum(data[which(data$journey_id == journeys[j]),8])
-  }
-}
-
-#distanza percorsa al giorno per ogni persona
-dist.per.day / people.per.day #troppo alta 
-
-#velocità dei viaggi (eventualmente si può fare anche caso functional: selezioniamo dei viaggi a caso (oppure gli scegliamo secondo certi criteri tipo giorno/notte ...) e calcoliamo la velocità istanzanea nei vari istanti di tempo)
-journeys <- unique(data.red$journey_id)
-space <-  NULL
-time <- NULL
-for(i in 1:length(journeys)){
-  space[i] <- sum(data[which(data$journey_id == journeys[i]),8])
-  time[i] <- sum(data[which(data$journey_id == journeys[i]),7])
-}
-vel <- space/time #velocità media di ogni viaggio
-
-#tempo medio al giorno dei viaggi
-time.per.day <- rep(0,7)
-for (i in 1:7){
-  jour <- unique(data[which(data$timestamp>=range.time[1]+(i-1)*24*60*60 & data$timestamp<range.time[1]+i*24*60*60),dim(data)[2]])
-  for(j in 1:length(jour)){
-    time.per.day[i] <- time.per.day[i] + sum(data[which(data$journey_id == jour[j]),7])
-    if(j==length(jour))
-      time.per.day[i] <- time.per.day[i]/length(jour)
-  }
-}
-#anche la durata media dei viaggi sembra un po' alta
+sum(dati$timedelta[126:223])
+# prove------
+# #"timestamp"  "cuebiq_id" "device"  "latitude" "longitude" "datetime"  "stopping"  "journey_id" "stationary_id"
+# dati_totaltime <- dati %>% select(time=first(timestamp ), tot_time= sum(timedelta),journey_id, cuebiq_id ,device , lat_init=first( latitude),long_init= first(longitude),time_init=first(datetime)) 
+#                     ... %>% group_by(cuebiq_id,journey_id)
+# timedelta <- as.integer(timedelta)
+# dati_totaltime <- dati %>% select( cuebiq_id,journey_id,tot_time=sum(as.numeric(timedelta)))%>% group_by(cuebiq_id,journey_id)
+# 
+# dati_totaltime <- dati %>%  group_by(cuebiq_id,journey_id) %>% summarise(tot_time = sum(timedelta))
+# which(is.na(timedelta)==TRUE)
+# 
+# dati <- dati[,-11]
+# 
+# dati <- dataset[,-dim(dataset)[2]]
+# dati[which(is.na(dati[,7])==TRUE),7] <- 0
+# dati[which(is.na(dati[,8])==TRUE),8] <- 0
+#####################################################################################
